583ea9c751702a82c7be7db682ea81a391c7b03f
elimerl <32183908+elimerl@users.noreply.github.com>
2020-09-16 16:00:19 -0700

    edit test and fix package.json license

--- test/test.js
+++ test/test.js
@@ -3,10 +3,13 @@ var assert = require('chai').assert,
 	beverages = {
 		tea: ['chai', 'matcha', 'oolong']
 	};
-it('Testing', () => {
+it('foo', () => {
 	assert.typeOf(foo, 'string'); // without optional message
 	assert.typeOf(foo, 'string', 'foo is a string'); // with optional message
 	assert.equal(foo, 'bar', 'foo equal `bar`');
 	assert.lengthOf(foo, 3, 'foo`s valsue has a length of 3');
+
+})
+it('tea', () => {
 	assert.lengthOf(beverages.tea, 3, 'beverages has 3 types of tea');
 })
