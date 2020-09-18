fe22c5cb22e2198bd8230c59957695dac0ca6a6c
elimerl <32183908+elimerl@users.noreply.github.com>
2020-09-17 17:48:37 -0700

    Added support for ImageMagick pre 7

--- server.js
+++ server.js
@@ -14,7 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>. 
  */
-const im = require('./lib/im')
+const im = require('./lib/im7')
 
 const express = require('express')
 const rateLimit = require('express-rate-limit');
